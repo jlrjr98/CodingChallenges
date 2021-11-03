67426052606f0b76eaa53382d07ac276d13ccb52
jlrjr98 <jlrjr98@gmail.com>
2021-11-03 15:10:31 -0500

    corrected javascript 2 problem

--- 11-03-21_Week/mimicRightShift.js
+++ 11-03-21_Week/mimicRightShift.js
@@ -1,7 +1,17 @@
+console.log(shiftToTheRight(-512, 10))
+
 function shiftToTheRight(num, places) {
+
+    if (places == 1) {
+        if (num % 2 == 0) return num/2;
+        else return Math.floor(num/2);
+    }
 
     let str = num.toString(2);
     str = str.substring(0, str.length - places);
 
-    return parseInt(str, 2);
+    result = parseInt(str, 2);
+
+    if (result.toString() == "NaN") return -1;
+    else return result;
 }
